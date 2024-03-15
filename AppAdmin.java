9a2e6e3332703a8230856a5ae36e4016718dc384
marwanwahmansio <144268508+marwanwahmansio@users.noreply.github.com>
2024-03-15 16:52:52 +0100

    Add files via upload

--- AppAdmin.java
+++ AppAdmin.java
@@ -1,22 +1,24 @@
 package fr.voxi.admin;
+import java.time.LocalDate;
 
 public class AppAdmin {
-	public static void main(String[] args) {
+		public static void main(String[] args) {
+			
+		Abonnement abn1 = new Abonnement(201, LocalDate.of(2024,1,3)) ;
+		Abonnement abn2 = new Abonnement(202, LocalDate.of(2024,1,10)) ;
 		
-		Oeuvre oeuvre103= new Oeuvre(103,"Le Crépuscule et l'Aube");
-		System.out.println(oeuvre103.toString());
-		System.out.println(Oeuvre.getNbOeuvre());
+		System.out.println (abn1.toString());
+		System.out.println (abn2.toString());
+		System.out.println ("La durée d'un abonnement est de "+Abonnement.getDureeAbn()+" mois");
+		Abonnement.setDureeAbn(5) ;
+		System.out.println ("La durée d'un abonnement est de "+Abonnement.getDureeAbn()+" mois");
 		
-		Oeuvre oeuvre302= new Oeuvre(302,"Nature Humaine");
-		System.out.println(oeuvre302.toString());
-		System.out.println(Oeuvre.getNbOeuvre());
-		
-		Oeuvre oeuvre318= new Oeuvre(318,"L'Anomalie");
-		System.out.println(oeuvre318.toString());
-		System.out.println(Oeuvre.getNbOeuvre());
-		
-		System.out.println(oeuvre103.getNbOeuvre());
-		
-		
-	}
+		abn1.setDureeAbn1(7);
+		System.out.println ("La durée de l'abonnement numéro 201 est de "+abn1.getDureeAbn1()+" mois");
+		System.out.println ("La durée de l'abonnement numéro 202 est de "+abn2.getDureeAbn2()+" mois");
+		System.out.println (Abonnement.getDureeAbn()) ;
+		}
+	
+	
+	
 }
